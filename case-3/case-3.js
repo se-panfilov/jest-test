6b2fc51ffd464165d8a9ce56908dd41022c96eab
Sergei Panfilov <se-panfilov@ya.ru>
2022-07-28 15:00:13 +0200

    case 4

--- case-3/case-3.js
+++ case-3/case-3.js
@@ -1,8 +1,8 @@
 import { div, sum } from './helper';
 import { message, toUpperCase } from './helper2';
 
-//Task: Assume that for some reason we cannot launch functions from "helper" and "helper2".\
-// Mock that functions and test the code
+//Task: Assume that for some reason we cannot launch functions from "helper" and "helper2".
+// Mock that functions and test the code.
 
 export function main(a, b) {
   return sum(a, b) + div(a, b);
---
+++ case-4/case-4.js
@@ -0,0 +1,23 @@
+// Task: create unit tests for "sum" and for "div".
+// hint: "div" might be hard to test, so it should be rewritten.
+
+export class Case4 {
+  a = 0;
+  b = 0;
+  c = 2;
+  d = 0;
+
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+    this.d = Math.random();
+  }
+
+  sum() {
+    return this.a + this.b;
+  }
+
+  div() {
+    return this.c / this.d;
+  }
+}
